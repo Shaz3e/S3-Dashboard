664ff55776cd1baeccb63cb3020a8e2dc65f6609
Shahrukh Akhtar <iam@shaz3e.com>
2024-10-17 18:03:30 +0500

    "Fix typo in view name and remove unused dasboard.blade.php file"

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -12,6 +12,6 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-       return view('admin.dasboard');
+       return view('admin.dashboard');
     }
 }
